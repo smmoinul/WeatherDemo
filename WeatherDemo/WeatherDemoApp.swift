1723ef88afbd02d6892ba0b7e03b500a61c16491
S M Moinul Islam <smmoinuli@gmail.com>
2025-10-07 19:28:02 +0600

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by S M Moinul Islam on 8/8/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherDemoTests/WeatherDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoTests.swift
+//  WeatherDemoTests
+//
+//  Created by S M Moinul Islam on 8/8/25.
+//
+
+import Testing
+@testable import WeatherDemo
+
+struct WeatherDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
